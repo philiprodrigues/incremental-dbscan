5d6cf8609cf08fc4f097b815b66dd0c69584c5ea
Philip Rodrigues <rodriges@fnal.gov>
2021-06-04 10:20:42 +0100

    First, non-working, attempt at one-by-one dbscan

--- dbscan.h
+++ dbscan.h
@@ -8,6 +8,7 @@
 
 #include "TGraph.h"
 #include "TCanvas.h"
+#include "TAxis.h"
 
 const int kNoise=-2;
 const int kUndefined=-1;
@@ -16,11 +17,12 @@ const int kUndefined=-1;
 struct Hit
 {
     Hit(float _time, int _chan)
-        : time(_time), chan(_chan), cluster(kUndefined)
+        : time(_time), chan(_chan), cluster(kUndefined), is_core(false), needs_cluster_reachable(false)
         {}
 
     float time;
     int chan, cluster;
+    bool is_core, needs_cluster_reachable;
 };
 
 //======================================================================
@@ -50,10 +52,10 @@ std::vector<std::vector<Hit*> > dbscan_orig(vector<Hit*>& hits, float eps, unsig
     std::vector<std::vector<Hit*> > ret;
 
     int cluster=-1; // The index of the current cluster
-    
+
     for(auto& p: hits){
         if(p->cluster!=kUndefined) continue; // We already did this one
-      
+
         std::vector<Hit*> nbr=neighbours(hits, *p, eps);
 
         if(nbr.size()<minPts){
@@ -111,9 +113,10 @@ std::vector<Hit*> neighbours_sorted(const vector<Hit*>& hits, const Hit& q, floa
 {
     std::vector<Hit*> ret;
     auto time_comp_lower=[](const Hit* hit, const float t) { return hit->time < t; };
-    auto time_comp_upper=[](const float t, const Hit* hit) { return hit->time < t; };
+
     auto start_it=std::lower_bound(hits.begin(), hits.end(), q.time - eps, time_comp_lower);
-    auto end_it=std::upper_bound(hits.begin(), hits.end(), q.time + eps, time_comp_upper);
+    auto end_it=std::lower_bound(hits.begin(), hits.end(), q.time + eps, time_comp_lower);
+    // std::cout << hits.size() << " " << std::distance(hits.begin(), start_it) << " " << std::distance(hits.begin(), end_it) << std::endl;
     for(auto hit_it=start_it; hit_it!=end_it; ++hit_it){
         if(manhattanDist(*(*hit_it), q)<=eps){
             ret.push_back(*hit_it);
@@ -124,16 +127,16 @@ std::vector<Hit*> neighbours_sorted(const vector<Hit*>& hits, const Hit& q, floa
 
 //======================================================================
 //
-// Modified DBSCAN algorithm that assumes the input in sorted by time
+// Modified DBSCAN algorithm that assumes the input is sorted by time
 std::vector<std::vector<Hit*> > dbscan_sorted_input(vector<Hit*>& hits, float eps, unsigned int minPts)
 {
     std::vector<std::vector<Hit*> > ret;
 
     int cluster=-1; // The index of the current cluster
-    
+
     for(auto& p: hits){
         if(p->cluster!=kUndefined) continue; // We already did this one
-      
+
         std::vector<Hit*> nbr=neighbours_sorted(hits, *p, eps);
 
         if(nbr.size()<minPts){
@@ -184,6 +187,115 @@ std::vector<std::vector<Hit*> > dbscan_sorted_input(vector<Hit*>& hits, float ep
     return ret;
 }
 
+struct State
+{
+    std::vector<Hit*> hits; // All the hits we've seen so far, in time order
+    int cluster{-1}; // The index of the current cluster
+    float latest_time{0}; // The latest time of a hit in the vector of hits
+    int last_processed_index{0}; // The last index in `hits` that we processed
+};
+
+//======================================================================
+void cluster_reachable(State& state, std::vector<Hit*> seedSet, int cluster, float eps, unsigned int minPts)
+{
+    // Loop over all neighbours (and the neighbours of core points, and so on)
+    while(!seedSet.empty()){
+        Hit* q=seedSet.back();
+        seedSet.pop_back();
+        if(q->time + eps > state.latest_time){
+            q->needs_cluster_reachable=true;
+            std::cout << "  hit at " << q->time << ", " << q->chan << " is within eps (" << eps << ") of latest time " << state.latest_time << std::endl;
+            continue;
+        }
+
+        // Change noise to a border point
+        if(q->cluster==kNoise){
+            std::cout << "  adding previously-noise hit at " << q->time << ", " << q->chan << " to cluster " << state.cluster << std::endl;
+            q->cluster=cluster;
+        }
+        if(q->cluster!=kUndefined) continue;
+        std::cout << "  adding previously-undefined hit at " << q->time << ", " << q->chan << " to cluster " << state.cluster << std::endl;
+        q->cluster=cluster;
+        // Neighbours of q
+        std::vector<Hit*> nbrq=neighbours_sorted(state.hits, *q, eps);
+        // If q is a core point, add its neighbours to the search list
+        if(nbrq.size()>=minPts){
+            q->is_core=true;
+            seedSet.insert(seedSet.end(), nbrq.begin(), nbrq.end());
+            std::cout << "  hit at " << q->time << ", " << q->chan << " has " << nbrq.size() << " neighbours, so is core" << std::endl;
+        }
+        else{
+            std::cout << "  hit at " << q->time << ", " << q->chan << " has " << nbrq.size() << " neighbours, so is *not* core" << std::endl;
+        }
+    }
+}
+
+//======================================================================
+//
+// Modified DBSCAN algorithm that takes one hit at a time, with the requirement that the hits are passed in time order
+void dbscan_partial_add_one(State& state, Hit* hit, float eps, unsigned int minPts)
+{
+    if(hit->cluster != kUndefined){
+        throw std::runtime_error("Hit cluster is already set");
+    }
+    state.latest_time = hit->time;
+
+    for(auto h: state.hits){
+        if(h->needs_cluster_reachable && h->time+eps < state.latest_time-1){
+            std::cout << "Repeche hit at " << h->time << ", " << h->chan << std::endl;
+            std::vector<Hit*> seed{h};
+            cluster_reachable(state, seed, h->cluster, eps, minPts);
+            h->needs_cluster_reachable=false;
+        }
+    }
+
+    state.hits.push_back(hit);
+    
+    // Walk process_until_index forward until it reaches the last hit
+    // that is a distance eps or more from the latest hit: for this
+    // point (and all those before it), all of the hit's
+    // eps-neighbours are already available
+    int process_until_index=0;
+    while(process_until_index<state.hits.size() && state.hits[process_until_index]->time<state.latest_time - eps) ++process_until_index;
+
+    std::cout << state.hits.size() << ". Got hit with time " << hit->time << ". Processing hits from index " << state.last_processed_index << " to " << process_until_index << std::endl;
+    for(int i=state.last_processed_index; i<process_until_index; ++i){
+        // Do regular dbscan on the items we haven't processed yet. An item found to be a non-core point here will never be a core point
+        Hit* p=state.hits[i];
+        if(p->cluster!=kUndefined) continue; // We already did this one
+        std::cout << "  hit " << i << " is kUndefined" << std::endl;
+        std::vector<Hit*> nbr=neighbours_sorted(state.hits, *p, eps);
+
+        if(nbr.size()<minPts){
+            // Not enough neighbours to be a core point. Classify as noise (but we might reclassify later)
+            std::cout << "  not enough hits to be a core point" << std::endl;
+            p->cluster=kNoise;
+            continue;
+        }
+
+        state.cluster++;
+        // Assign this core point to the current cluster
+        p->cluster=state.cluster;
+        p->is_core=true;
+        // Seed set is all the neighbours of p except for p
+        std::vector<Hit*> seedSet;
+        for(auto const& n: nbr){
+            if(n!=p){
+                seedSet.push_back(n);
+            }
+        }
+        if(seedSet.size()!=nbr.size()-1){
+            std::cout << "seedSet.size()=" << seedSet.size() << " but nbr.size()=" << nbr.size() << std::endl;
+        }
+        assert(seedSet.size()==nbr.size()-1);
+
+        std::cout << "  " << nbr.size() << " neighbours, which is enough to be a core point, so looping over proper neighbours" << std::endl;
+        cluster_reachable(state, seedSet, state.cluster, eps, minPts);
+    }
+
+    state.last_processed_index=process_until_index;
+}
+
 //======================================================================
 void draw_clusters(const vector<Hit*>& hits)
 {
@@ -196,11 +308,15 @@ void draw_clusters(const vector<Hit*>& hits)
     int colIndex=0;
 
     for(auto const& hit: hits){
-        grAll->SetPoint(grAll->GetN(), hit->chan, hit->time);
+        grAll->SetPoint(grAll->GetN(), hit->time, hit->chan);
         if(grs.find(hit->cluster)==grs.end()){
             TGraph* gr=new TGraph;
             if(hit->cluster==kNoise){
                 gr->SetMarkerColor(kGray);
+            }
+            if(hit->cluster==kUndefined){
+                gr->SetMarkerColor(kGray+2);
+                gr->SetMarkerStyle(2);
             }
             else{
                 gr->SetMarkerColor(colours[(colIndex++)%nColours]);
@@ -209,9 +325,11 @@ void draw_clusters(const vector<Hit*>& hits)
             grs[hit->cluster]=gr;
         }
         TGraph* gr=grs[hit->cluster];
-        gr->SetPoint(gr->GetN(), hit->chan, hit->time);
+        gr->SetPoint(gr->GetN(), hit->time, hit->chan);
     }
     grAll->Draw("ap");
+    grAll->GetXaxis()->SetTitle("Time");
+    grAll->GetYaxis()->SetTitle("Channel");
     for(auto const& gr: grs){
         gr.second->Draw("p");
     }
--- test_dbscan.C
+++ test_dbscan.C
@@ -3,28 +3,43 @@
 
 #include <thread>
 #include <chrono>
+#include <fstream>
+
+std::vector<Hit*> get_hits(std::string name)
+{
+    std::vector<Hit*> hits;
+    if(name=="simple"){
+        hits.push_back(new Hit(8.3, 101));
+        hits.push_back(new Hit(2.6, 103));
+        hits.push_back(new Hit(5.3, 104));
+        hits.push_back(new Hit(6.1, 105));
+        hits.push_back(new Hit(6.8, 106));
+        hits.push_back(new Hit(7.3, 107));
+        hits.push_back(new Hit(7.9, 108));
+        hits.push_back(new Hit(8.0, 109));
+        hits.push_back(new Hit(8.7, 110));
+        hits.push_back(new Hit(16.1, 105));
+        hits.push_back(new Hit(16.8, 106));
+        hits.push_back(new Hit(17.3, 107));
+        hits.push_back(new Hit(17.9, 108));
+        hits.push_back(new Hit(18.0, 109));
+        hits.push_back(new Hit(18.7, 110));
+    }
+    else{
+        std::ifstream fin(name);
+        uint64_t timestamp, first_timestamp{0};
+        int channel;
+        while(fin >> channel >> timestamp){
+            if(first_timestamp==0) first_timestamp=timestamp;
+            hits.push_back(new Hit((timestamp-first_timestamp)/100, channel));
+        }
+    }
+    return hits;
+}
 
 void test_dbscan()
 {
-    std::vector<Hit*> hits;
-    hits.push_back(new Hit(8.3, 101));
-    hits.push_back(new Hit(2.6, 103));
-    hits.push_back(new Hit(5.3, 104));
-    hits.push_back(new Hit(6.1, 105));
-    hits.push_back(new Hit(6.8, 106));
-    hits.push_back(new Hit(7.3, 107));
-    hits.push_back(new Hit(7.9, 108));
-    hits.push_back(new Hit(8.0, 109));
-    hits.push_back(new Hit(8.7, 110));
-
-
-    hits.push_back(new Hit(16.1, 105));
-    hits.push_back(new Hit(16.8, 106));
-    hits.push_back(new Hit(17.3, 107));
-    hits.push_back(new Hit(17.9, 108));
-    hits.push_back(new Hit(18.0, 109));
-    hits.push_back(new Hit(18.7, 110));
-
+    auto hits=get_hits("felix501_off_30.txt");
 
     TStopwatch ts;
     dbscan_orig(hits, 5, 2);
@@ -34,8 +49,25 @@ void test_dbscan()
 
     std::vector<Hit*> hits_sorted(hits);
     std::sort(hits.begin(), hits.end(), [](Hit* a, Hit* b) { return a->time < b->time; });
-    dbscan_sorted_input(hits_sorted, 5, 2);
-    draw_clusters(hits_sorted);
+    // for(auto h: hits_sorted) h->cluster=kUndefined;
+    // dbscan_sorted_input(hits_sorted, 5, 2);
+    // draw_clusters(hits_sorted);
 
+    for(auto h: hits_sorted) h->cluster=kUndefined;
+    State state;
+    for(auto h: hits_sorted){
+        dbscan_partial_add_one(state, h, 5, 2);
+    }
+
+    // Give it a far-future hit so it goes through all of the hits
+    Hit future_hit(10000, 110);
+    dbscan_partial_add_one(state, &future_hit, 5, 2);
+    draw_clusters(hits_sorted);
     
 }
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// c-file-style: "linux"
+// End:
